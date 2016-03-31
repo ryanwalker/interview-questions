ce3a34431ac1a2c6c7af3e0f7bf98da4e20e1ab6
Ryan Walker <ryanwalker7@gmail.com>
2016-03-31 16:29:22 -0700

    Doc updates

--- intern/src/main/java/factory/MessageSender.java
+++ intern/src/main/java/factory/MessageSender.java
@@ -2,8 +2,8 @@ package factory;
 
 /**
  * Instructions:
- * Create 3 Messages: email, text and push (like an iOS or Android push notification)
- * Do not use concrete Message constructors in this class. Instead, use the Factory pattern to create messages.
+ * Create 3 classes that implement the Messages interface: email, text and push (iOS or Android push notification)
+ * Do not use concrete Message constructors in the main method. Instead, use the MessageFactory class and Factory pattern to create messages.
  *
  * Objectives
  * Demonstrate polymorphism, encapsulation and proper use of access modifiers
@@ -11,13 +11,16 @@ package factory;
  * Output:
  * Write the following to standard out:
  * Using EmailFactory to create this message...
- * Sending the following email to fred@gmail.com:\nFred, your house closes on Monday.
+ * Sending the following email to fred@gmail.com:
+ * Fred, your house closes on Monday.
  *
  * Using TextFactory to create this message...
- * Sending the following text to 4809998765@tmobtxt.com:\nAre we going to dinner tonight? :)
+ * Sending the following text to 4809998765@tmobtxt.com:
+ * Are we going to dinner tonight? :)
  *
  * Using PushFactory to create this message...
- * Sending the following push notification to jeffrey@me.com:\nYou have 3 new leads, nice work!
+ * Sending the following push notification to jeffrey@me.com:
+ * You have 3 new leads, nice work!
  */
 public class MessageSender {
 
